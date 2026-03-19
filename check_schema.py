21e2eed77ecb3318430ed96ad6438a3d24206de9
uday <uday.dewani1098@gmail.com>
2026-03-19 14:55:35 +0530

    moved model evaluation files to test folder

--- check_schema.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-import json
-url = "https://datasets-server.huggingface.co/info?dataset=ComplexDataLab/OpenFake"
-response = urllib.request.urlopen(url)
-data = json.loads(response.read().decode())
-label_feature = data["dataset_info"]["train"]["features"]["label"]
-print(f"Label Feature: {label_feature}")
